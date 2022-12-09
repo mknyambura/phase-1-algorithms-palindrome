31f5a48eae468ac98fdd91fdb5709df5f1703a8b
mknyambura <kariuki.mercie@gmail.com>
2022-12-09 11:58:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,14 +11,17 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
-  
+  check if word length is less than or equal to 1
+
+  if the first letter of the word is the same as the last letter of the word
+    slice the word and heck again
 
 */
 
 /*
   Add written explanation of your solution here
   -If the length of the word is 1 letter or less than 1, it's a palindrome.
-  -If the first letter of the word is the same as the last letter, then slice the wordd and check again until completion
+  -If the first letter of the word is the same as the last letter, then slice the word and check again until completion
 */
 
 // You can run `node index.js` to view these console logs
